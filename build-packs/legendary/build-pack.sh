45f320d36d176d9e98df65aadefcc8c074aafe09
Tom Rodriguez <tmrodrig@gmail.com>
2024-12-10 20:22:10 -0800

    fix access to unique dir

--- build-packs/legendary/build-pack.sh
+++ build-packs/legendary/build-pack.sh
@@ -2,6 +2,7 @@
 PACK=$1
 PACKBASE=../../assets/packs/legendary/$PACK
 DATADIR=$PACKBASE/data
+UNIQUEDIR=$PACKBASE/unique
 BUILDDIR=build/$PACK
 PACKDIR=../../packs
 GENFILE=generate-$PACK.py
@@ -11,7 +12,7 @@ PACKTYPE=$(grep "^type=" $PACKBASE/pack.properties| cut -d= -f2)
 mkdir -p $BUILDDIR
 python3 ./generate-$PACK.py $DATADIR $BUILDDIR
 if [ $? -eq 0 ]; then
-    [ -d unique -a ! -z "$( ls -A $PACKBASE/unique/*.json 2>/dev/null )" ] && cp $PACKBASE/unique/*.json build
+    [ -d $UNIQUEDIR -a ! -z "$( ls -A $UNIQUEDIR/*.json 2>/dev/null )" ] && cp $UNIQUEDIR/*.json $BUILDDIR
     fvtt package pack -n $PACK -v --type System --id sohl -t $PACKTYPE --in $BUILDDIR --out $PACKDIR
 else
     echo -e "\033[0;31mERROR:\033[0m Build Failed!!"
